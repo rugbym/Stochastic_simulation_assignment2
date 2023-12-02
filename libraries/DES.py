f44a37f1070a57b7c057c9ebdf1dd47fe746003d
dosbrunos <94176523+dosbrunos@users.noreply.github.com>
2023-12-02 15:46:36 +0100

    Update DES.py

--- libraries/DES.py
+++ libraries/DES.py
@@ -7,12 +7,14 @@ wait_times = []
 
 
 class Theater(object):
-    def __init__(self, env, num_cashiers):
+    def __init__(self, env, num_cashiers,mu):
         self.env = env
         self.cashier = simpy.Resource(env, num_cashiers)
+        self.mu = mu
+
 
     def purchase_ticket(self, moviegoer):
-        yield self.env.timeout(random.randint(1, 3))
+        yield self.env.timeout(random.expovariate(self.mu))
 
 
 def go_to_movies(env, moviegoer, theater):
@@ -28,14 +30,14 @@ def go_to_movies(env, moviegoer, theater):
     wait_times.append(env.now - arrival_time)
 
 
-def run_theater(env, num_cashiers):
-    theater = Theater(env, num_cashiers)
+def run_theater(env, num_cashiers, lamda, mu):
+    theater = Theater(env, num_cashiers, lamda)
 
     for moviegoer in range(3):
         env.process(go_to_movies(env, moviegoer, theater))
 
     while True:
-        yield env.timeout(0.20)  # Wait a bit before generating a new person
+        yield env.timeout(random.expovariate(lamda))  # Wait a bit before generating a new person
 
         moviegoer += 1
         env.process(go_to_movies(env, moviegoer, theater))
@@ -61,10 +63,13 @@ num_server = [1,2,4]
 for cashiers in num_server:
     # Setup
     num_cashiers = cashiers
+    lamda=1
+    mu =1
+
 
     # Run the simulation
     env = simpy.Environment()
-    env.process(run_theater(env, num_cashiers))
+    env.process(run_theater(env, num_cashiers, lamda, mu))
     env.run(until=90)
 
     # View the results
